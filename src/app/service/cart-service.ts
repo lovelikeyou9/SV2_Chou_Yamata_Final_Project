f7f60ec5a92bcb92071ffa3e48a7a492ce8da180
Mata <lovelikeyou9@gmail.com>
2025-08-21 12:25:40 +0700

    Update 0.2

--- src/app/service/cart-service.ts
+++ src/app/service/cart-service.ts
@@ -59,3 +59,74 @@ export class CartService {
 
 
 }
+
+
+/*
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  constructor() {
+    this.cart = JSON.parse(localStorage.getItem('carts') ?? '[]');
+  }
+
+  private cart: any = [];
+
+  addItem(item: any) {
+    let current_cart = JSON.parse(localStorage.getItem('carts') ?? '[]');
+    let dpl_index: number = this.cart.findIndex((obj: any) => obj.id == item.id);
+    if (dpl_index > -1) {
+      current_cart[dpl_index].qty +=1;
+      console.log(current_cart);
+      localStorage.setItem('carts', JSON.stringify(current_cart));
+    }
+    else {
+      item.qty = 1;
+      this.cart.push(item);
+      current_cart.push(item);
+      localStorage.setItem('carts', JSON.stringify(current_cart));
+    }
+    
+
+  }
+
+
+  getItem(): any {
+    return JSON.parse(localStorage.getItem('carts') ?? '[]');
+  }
+
+  getTotal(): any {
+    let total = 0;
+    this.cart.forEach((item: { price: number; qty: number; }) => {
+      total += item.price * item.qty;
+    });
+    return total;
+  }
+
+  removeItem(item: any){
+    this.cart.splice(this.cart.indexOf(item), 1);
+    localStorage.setItem('carts', JSON.stringify(this.cart));
+  }
+
+  incrementQty(item: any){
+    let index = this.cart.indexOf(item);
+    if(index > -1){
+      this.cart[index].qty++;
+      localStorage.setItem('carts', JSON.stringify(this.cart));
+    }
+  }
+
+  decrementQty(item: any){
+    let index = this.cart.indexOf(item);
+    if(index > -1){
+      this.cart[index].qty--;
+      localStorage.setItem('carts', JSON.stringify(this.cart));
+    }
+  }
+
+
+}
+
+*/
